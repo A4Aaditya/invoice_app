d280d7e16dfa6ff5b8b0692e34dce48a2e338100
Aditya Kumar Singh <107492573+A4Aaditya@users.noreply.github.com>
2025-07-02 03:10:35 +0530

    Revert "Added Supabase to the Project (#2)"
    This reverts commit 2950e39465e5cc46b138d9c41a1209a16cfe3d8e.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:invoice_app/app.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await setupSupabase();
+void main() {
   runApp(const MyApp());
-}
-
-Future<void> setupSupabase() async {
-  const url = String.fromEnvironment('SUPABASE_URL');
-  const anonKey = String.fromEnvironment('ANON_KEY');
-  assert(url.isNotEmpty, "SUPABASE_URL is missing");
-  assert(anonKey.isNotEmpty, "ANON_KEY is missing");
-  await Supabase.initialize(url: url, anonKey: anonKey);
 }

042bae8e5d5f341f7037619e2198774614dfa100
Aditya Kumar Singh <107492573+A4Aaditya@users.noreply.github.com>
2025-07-02 01:06:20 +0530

    Added Supabase to the Project (#2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:invoice_app/app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupSupabase();
   runApp(const MyApp());
+}
+
+Future<void> setupSupabase() async {
+  const url = String.fromEnvironment('SUPABASE_URL');
+  const anonKey = String.fromEnvironment('ANON_KEY');
+  assert(url.isNotEmpty, "SUPABASE_URL is missing");
+  assert(anonKey.isNotEmpty, "ANON_KEY is missing");
+  await Supabase.initialize(url: url, anonKey: anonKey);
 }

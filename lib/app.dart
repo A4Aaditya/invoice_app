9d20f9a6fdde5f7394c43a5c5d243c96eb89d2c0
Aditya Kumar Singh <107492573+A4Aaditya@users.noreply.github.com>
2025-07-02 03:10:35 +0530

    Revert "Added Auth (#3)"
    This reverts commit 7f0f896341e6b4fd5b4ac2e172c7e760767da147.

--- lib/app.dart
+++ lib/app.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:invoice_app/i18n/app_localizations.dart';
-import 'package:invoice_app/pages/dashboard_screen.dart';
 import 'package:invoice_app/pages/splash_screen.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final currentUser = Supabase.instance.client.auth.currentUser;
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: currentUser == null
-          ? const SplashScreen()
-          : const DashboardScreen(),
+      home: SplashScreen(),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
-      locale: const Locale('en'),
+      locale: Locale('en'),
       supportedLocales: AppLocalizations.supportedLocales,
     );
   }

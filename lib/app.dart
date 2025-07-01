728a8fc06bc2e37c22f52890f6fcf74685c8e902
A4Aaditya <adityakumarsinghlbh@gmail.com>
2025-07-02 02:12:32 +0530

    Added Auth

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:invoice_app/i18n/app_localizations.dart';
+import 'package:invoice_app/pages/dashboard_screen.dart';
 import 'package:invoice_app/pages/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final currentUser = Supabase.instance.client.auth.currentUser;
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: currentUser == null
+          ? const SplashScreen()
+          : const DashboardScreen(),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
-      locale: Locale('en'),
+      locale: const Locale('en'),
       supportedLocales: AppLocalizations.supportedLocales,
     );
   }

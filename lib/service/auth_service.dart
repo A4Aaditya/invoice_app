ed2ecf9a60dc43cfc48c955e9a6dcadb5faa8902
Aditya Kumar Singh <107492573+A4Aaditya@users.noreply.github.com>
2025-07-02 16:52:19 +0530

    Feature/fl 25 add service for auth (#11)
    * created auth service and auth service response
    
    * improved the auth serive and create auth params
    
    * added auth service to get it
    
    * removed  unused params
    
    * taking auth instance from get it

---
+++ lib/service/auth_service.dart
@@ -0,0 +1,62 @@
+import 'package:get_it/get_it.dart';
+import 'package:invoice_app/entities/auth_params/login_params.dart';
+import 'package:invoice_app/entities/auth_params/no_params.dart';
+import 'package:invoice_app/entities/auth_params/register_params.dart';
+import 'package:invoice_app/model/auth_service_response.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+  final supabaseClient = GetIt.instance.get<SupabaseClient>();
+
+  Future<AuthServiceResponse> signinWithPassword(
+    LoginParams loginParams,
+  ) async {
+    try {
+      final response = await supabaseClient.auth.signInWithPassword(
+        password: loginParams.password,
+        email: loginParams.email,
+      );
+
+      return AuthServiceResponse(
+        session: response.session,
+        user: response.user,
+      );
+    } on AuthException catch (e) {
+      return AuthServiceResponse(
+        message: e.message,
+        statusCode: e.statusCode,
+        code: e.code,
+      );
+    } catch (e) {
+      return AuthServiceResponse();
+    }
+  }
+
+  Future<AuthServiceResponse> registerEmailPassword(
+    RegisterParams registerParams,
+  ) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        password: registerParams.password,
+        email: registerParams.email,
+      );
+
+      return AuthServiceResponse(
+        session: response.session,
+        user: response.user,
+      );
+    } on AuthException catch (e) {
+      return AuthServiceResponse(
+        message: e.message,
+        statusCode: e.statusCode,
+        code: e.code,
+      );
+    } catch (e) {
+      return AuthServiceResponse();
+    }
+  }
+
+  Future<void> signOut(NoParams noParams) async {
+    await supabaseClient.auth.signOut();
+  }
+}

0120990377992fc8dca17286632fda2deaaa0e12
A4Aaditya <adityakumarsinghlbh@gmail.com>
2025-07-02 15:36:08 +0530

    removed  unused params

--- lib/entities/auth_params/login_params.dart
+++ lib/entities/auth_params/login_params.dart
@@ -1,13 +1,6 @@
 class LoginParams {
   final String email;
-  final String? phone;
   final String password;
-  final String? captchaToken;
 
-  LoginParams({
-    required this.password,
-    required this.email,
-    this.captchaToken,
-    this.phone,
-  });
+  LoginParams({required this.password, required this.email});
 }

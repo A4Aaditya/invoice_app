9c3ba71050207c1accf282dedb39029b5324ecb7
A4Aaditya <adityakumarsinghlbh@gmail.com>
2025-07-02 23:26:15 +0530

    updated test

--- test/service/auth_service_test.dart
+++ test/service/auth_service_test.dart
@@ -8,26 +8,42 @@ class MockSupabaseClient extends Mock implements SupabaseClient {}
 
 class MockGoTrueClient extends Mock implements GoTrueClient {}
 
+class MockSession extends Mock implements Session {}
+
+class MockUser extends Mock implements User {}
+
+class MockAuthResponse extends Mock implements AuthResponse {}
+
 void main() {
   late final MockSupabaseClient mockSupabaseClient;
   late final AuthService authService;
-  late final MockGoTrueClient mockGoTrueClient;
+  late final MockGoTrueClient mockAuth;
 
   setUpAll(() {
     mockSupabaseClient = MockSupabaseClient();
-    mockGoTrueClient = MockGoTrueClient();
+    mockAuth = MockGoTrueClient();
     authService = AuthService(supabaseClient: mockSupabaseClient);
+    when(() => mockSupabaseClient.auth).thenReturn(mockAuth);
   });
-  test("register with email when email is empty", () async {
-    final params = RegisterParams(email: "", password: "password");
-    final fakeResponse = await mockGoTrueClient.signUp(
-      password: params.password,
-      email: params.email,
+
+  test("register with email and password", () async {
+    final params = RegisterParams(
+      email: "mockUser@gmail.com",
+      password: "password",
     );
+    final mockSession = MockSession();
+    final mockuser = MockUser();
+    final mockResponse = MockAuthResponse();
+    when(() => mockResponse.user).thenReturn(mockuser);
+    when(() => mockuser.email).thenReturn("mockUser@gmail.com");
+    when(() => mockResponse.session).thenReturn(mockSession);
+    when(
+      () => mockAuth.signUp(password: params.password, email: params.email),
+    ).thenAnswer((_) async => mockResponse);
+    final result = await authService.registerEmailPassword(params);
 
-    final response = await authService.registerEmailPassword(params);
-
-    expect(response.session, fakeResponse.session);
-    expect(response.user, fakeResponse.user);
+    expect(result.user, mockuser);
+    expect(result.session, mockSession);
+    expect(result.user?.email, "mockUser@gmail.com");
   });
 }

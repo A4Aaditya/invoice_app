ea7d9876b03d260d62d92ea291ba412a02a00bdf
A4Aaditya <adityakumarsinghlbh@gmail.com>
2025-07-02 15:35:39 +0530

    added auth service to get it

--- lib/utils/service_locator.dart
+++ lib/utils/service_locator.dart
@@ -1,7 +1,11 @@
 import 'package:get_it/get_it.dart';
+import 'package:invoice_app/service/auth_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void setupLocator() {
   final supabase = Supabase.instance.client;
   GetIt.instance.registerSingleton<SupabaseClient>(supabase);
+
+  final authService = AuthService();
+  GetIt.instance.registerSingleton<AuthService>(authService);
 }

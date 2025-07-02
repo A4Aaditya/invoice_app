8f947e2e5e0c0923a2bf029b050f2c35a00a4771
A4Aaditya <adityakumarsinghlbh@gmail.com>
2025-07-02 18:08:10 +0530

    added functionality to register screen and login screen

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:invoice_app/entities/auth_params/login_params.dart';
 import 'package:invoice_app/pages/dashboard_screen.dart';
+import 'package:invoice_app/pages/register_screen.dart';
 import 'package:invoice_app/service/auth_service.dart';
 import 'package:invoice_app/utils/snackbar.dart';
 import 'package:invoice_app/widgets/button.dart';
@@ -27,9 +28,22 @@ class _LoginScreenState extends State<LoginScreen> {
           InputField(controller: emailController),
           InputField(controller: passwordController),
           Button(onPressed: onSiginTap, child: const Text("Login")),
+          TextButton(
+            onPressed: onSignupTap,
+            child: const Text("SignUp", style: TextStyle(fontSize: 16)),
+          ),
         ],
       ),
     );
+  }
+
+  void onSignupTap() {
+    final route = MaterialPageRoute(
+      builder: (context) {
+        return const RegisterScreen();
+      },
+    );
+    Navigator.push(context, route);
   }
 
   Future<void> onSiginTap() async {
--- lib/pages/register_screen.dart
+++ lib/pages/register_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:invoice_app/entities/auth_params/register_params.dart';
+import 'package:invoice_app/pages/login_screen.dart';
+import 'package:invoice_app/service/auth_service.dart';
+import 'package:invoice_app/utils/snackbar.dart';
 import 'package:invoice_app/widgets/button.dart';
 import 'package:invoice_app/widgets/input_field.dart';
 
@@ -27,5 +32,28 @@ class _RegisterScreenState extends State<RegisterScreen> {
     );
   }
 
-  Future<void> register() async {}
+  Future<void> register() async {
+    final email = emailController.text.trim();
+    final password = passwordController.text.trim();
+    final registerParams = RegisterParams(email: email, password: password);
+    final authService = GetIt.instance.get<AuthService>();
+
+    final response = await authService.registerEmailPassword(registerParams);
+    if (response.user != null) {
+      if (!mounted) return;
+      SnackbarHelper.showSuccess(
+        context,
+        "User created successfully! Confirmation link sent to your register mail.",
+      );
+      navigateToLoginScreen();
+    } else {
+      if (!mounted) return;
+      SnackbarHelper.showError(context, response.message);
+    }
+  }
+
+  void navigateToLoginScreen() {
+    final route = MaterialPageRoute(builder: (context) => const LoginScreen());
+    Navigator.pushAndRemoveUntil(context, route, (route) => false);
+  }
 }

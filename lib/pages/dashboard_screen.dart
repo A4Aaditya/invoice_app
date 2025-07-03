2c8a361e1fc9dcb337c8e19e6a49af4f1990b717
A4Aaditya <adityakumarsinghlbh@gmail.com>
2025-07-04 01:28:46 +0530

    create ui and riverprod

--- lib/pages/dashboard_screen.dart
+++ lib/pages/dashboard_screen.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:invoice_app/riverpod/bottom_nav_index_provider.dart';
+import 'package:invoice_app/riverpod/screen_provider.dart';
+import 'package:invoice_app/widgets/bottom_nav.dart';
 
-class DashboardScreen extends StatefulWidget {
+class DashboardScreen extends ConsumerStatefulWidget {
   const DashboardScreen({super.key});
 
   @override
-  State<DashboardScreen> createState() => _DashboardScreenState();
+  ConsumerState<DashboardScreen> createState() => _DashboardScreenState();
 }
 
-class _DashboardScreenState extends State<DashboardScreen> {
+class _DashboardScreenState extends ConsumerState<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Dashboard Screen")));
+    final bottomNavRead = ref.read(bottomNavIndexProvider.notifier);
+
+    return Scaffold(
+      body: Consumer(
+        builder: (context, ref, child) {
+          return ref.watch(screenProviderByIndex).screen;
+        },
+      ),
+      bottomNavigationBar: BottomNav(
+        onHomeClicked: () {
+          bottomNavRead.changeIndex(0);
+        },
+
+        onInvoiceClicked: () {
+          bottomNavRead.changeIndex(1);
+        },
+        onTransactionClicked: () {
+          bottomNavRead.changeIndex(2);
+        },
+
+        onSettingClicked: () {
+          bottomNavRead.changeIndex(3);
+        },
+      ),
+    );
   }
 }

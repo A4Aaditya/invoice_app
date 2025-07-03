36cd608c8ae7d74cd0803b7accdba2bad6eac74d
A4Aaditya <adityakumarsinghlbh@gmail.com>
2025-07-03 19:06:12 +0530

    added test for the signin with pasword

--- test/service/auth_service_test.dart
+++ test/service/auth_service_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:invoice_app/entities/auth_params/login_params.dart';
 import 'package:invoice_app/entities/auth_params/register_params.dart';
 import 'package:invoice_app/model/service_response.dart';
 import 'package:invoice_app/service/auth_service.dart';
@@ -13,7 +14,9 @@ class MockUser extends Mock implements User {}
 
 class MockAuthResponse extends Mock implements AuthResponse {}
 
-class MockException extends Mock implements AuthException {}
+class MockAuthException extends Mock implements AuthException {}
+
+class MockException extends Mock implements Exception {}
 
 void main() {
   late final MockSupabaseClient mockSupabaseClient;
@@ -49,18 +52,54 @@ void main() {
 
   test("When register method throw exception", () async {
     final params = RegisterParams(email: "", password: "password");
+    final mockAuthException = MockAuthException();
     final mockException = MockException();
-    when(() => mockException.message).thenAnswer((_) => "User not found");
+
+    when(() => mockAuthException.message).thenAnswer((_) => "User not found");
+
+    when(
+      () => mockAuth.signUp(password: params.password, email: params.email),
+    ).thenThrow(mockAuthException);
+
     when(
       () => mockAuth.signUp(password: params.password, email: params.email),
     ).thenThrow(mockException);
+
     try {
       await authService.registerEmailPassword(params);
     } on AuthException catch (e) {
       expect(e, isA<FailureResult>());
       if (e case FailureResult<User, String>(:final error)) {
-        expect(error, mockException.message);
+        expect(error, mockAuthException.message);
       }
+    } on Exception catch (e) {
+      expect(e, isA<FailureResult>());
+      if (e case FailureResult<User, String>(:final error)) {
+        expect(error, mockAuthException.message);
+      }
+    }
+  });
+
+  test("signin with email and password   ", () async {
+    final params = LoginParams(password: "password", email: "mockUser@gmail");
+    final mockuser = MockUser();
+    final mockAuthResponse = MockAuthResponse();
+
+    when(() => mockAuthResponse.user).thenReturn(mockuser);
+
+    when(() => mockuser.email).thenReturn("mockUser@gmail");
+
+    when(
+      () => mockAuth.signInWithPassword(
+        password: params.password,
+        email: params.email,
+      ),
+    ).thenAnswer((_) async => mockAuthResponse);
+
+    final result = await authService.signinWithPassword(params);
+
+    if (result case SucessResult<User, String>(:final data)) {
+      expect(data, mockuser);
     }
   });
 }
